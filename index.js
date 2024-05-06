1ee214bf2062f380ea057b84d8471e0144e3114a
lucsmat15 <lucsssmat@gmail.com>
2024-05-05 21:45:46 -0300

    Projeto concluído

---
+++ index.js
@@ -0,0 +1,24 @@
+let nomeHeroi = "Lucas Matheus";
+let xpHeroi = 70000; // Altere este valor conforme necessário
+
+let nivelHeroi;
+
+if (xpHeroi < 1000) {
+    nivelHeroi = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+console.log(nomeHeroi + " está no nível de " + nivelHeroi);
